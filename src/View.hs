919549d98b610987668cdd14070afe16b39c0c1d
Raymon Zutekouw <40148684+Raymonzut@users.noreply.github.com>
2021-10-29 19:53:52 +0200

    Add View half* helpers

--- src/View.hs
+++ src/View.hs
@@ -1,5 +1,6 @@
 module View where
 
+import GHC.Float
 import Graphics.Gloss
 import Graphics.Gloss.Data.Point
 
@@ -7,6 +8,10 @@ import Model
 
 screenSize :: (Int, Int)
 screenSize = (240, 320)
+
+halfWidthOf, halfHeightOf :: (Int, Int) -> Float
+halfWidthOf = int2Float . (`div` 2) . fst
+halfHeightOf = int2Float . (`div` 2) . snd
 
 view :: GameState -> IO Picture
 view = return . viewPure
