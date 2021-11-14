9b32325e2070836df9b493f285c7bfe7f3c586cc
Raymon Zutekouw <40148684+Raymonzut@users.noreply.github.com>
2021-11-14 15:17:03 +0100

    Implement parse for game.log

--- src/LibHighScoreBoard.hs
+++ src/LibHighScoreBoard.hs
@@ -2,11 +2,16 @@ module LibHighScoreBoard where
 
 import Model (HighScoreBoard)
 
+import qualified Data.Map as Map
+import Data.Maybe
+import Data.List.Split
 import System.Directory
-
 
 hsSize :: Int
 hsSize = 5
+
+
+emptyScoreBoard = replicate hsSize ("...", 0)
 
 updateHighScoreBoard :: Int -> String -> IO HighScoreBoard
 updateHighScoreBoard pScore pName = do fileExist <- doesFileExist "game.log"
@@ -14,7 +19,12 @@ updateHighScoreBoard pScore pName = do fileExist <- doesFileExist "game.log"
                                        then return emptyScoreBoard
                                        else do content <- readFile "game.log"
                                                return $ parse content
-  where emptyScoreBoard = replicate hsSize ("...", 0)
 
 parse :: String -> HighScoreBoard
-parse = undefined
+parse blob = maybe emptyScoreBoard parseRows $ Map.lookup "highscore" parsedFile
+  where parsedFile = Map.fromList (map (tuple2 . splitOn ":") $ lines blob)
+        parseRows :: String -> HighScoreBoard
+        parseRows blob = map (\xs -> (head xs, (read . head $ tail xs) :: Int)) . chunksOf 2 $ splitOn ";" blob
+
+tuple2 :: [a] -> (a, a)
+tuple2 (x : y : _) = (x, y)
